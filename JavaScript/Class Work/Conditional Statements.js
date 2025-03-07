0706c618134b67d5aee0a88331e3156decf56efe
yash-ghodasara-91 <yashghodasara91@gmail.com>
2025-03-07 11:12:04 +0530

    Conditional Statements

---
+++ JavaScript/Class Work/Conditional Statements.js
@@ -0,0 +1,279 @@
+
+// data type
+
+// let a = 156456;
+
+// let b = "darshan"
+
+// let c = true
+
+// let d = Symbol("jjkash" , 54464)
+
+// let e = BigInt(123145645646)
+
+// console.log("Darshan");
+
+// console.log(a);
+
+// console.log(b);
+
+// console.log(c);
+
+// console.log(d);
+
+// console.log(e);
+
+// console.log(typeof(a));
+
+// console.log(typeof(b));
+
+// console.log(typeof(c));
+
+// console.log(typeof(d));
+
+// console.log(typeof(e));
+
+
+console.log("========================================================================");
+
+// operator
+
+// (1) assignment operator
+// = , += , -= , *= , /= , %= , **=
+
+// let a = 7;
+
+// console.log(a);
+
+// a += 5;
+// console.log(a);
+
+// a -= 5;
+// console.log(a);
+
+// a *= 2;
+// console.log(a);
+
+// a /= 2;
+// console.log(a);
+
+// a %= 2;
+// console.log(a);
+
+// a **= 2;
+// console.log(a);
+
+console.log("========================================================================");
+
+// (2) arithmatic operator
+// + , - , * , / , % , ++ , -- , **
+
+// let x = 4;
+// let y = 2;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);  
+// console.log(x % y);
+// console.log(++x);
+// console.log(--y);
+// console.log(x ** y);
+
+console.log("========================================================================");
+
+// (3) comparison operator
+// > , < , >= , <= , == , === , != , !==
+
+// let a = 5;
+// let b = 3;
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a === b);
+// console.log(a !== b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+
+console.log("========================================================================");
+
+// (4) logical operator
+// && , || , !
+
+// let a = 6;
+// let b = 3;
+
+// console.log(a < b && a === 6);
+// console.log(a < b || a === 6);
+// console.log(!(a < b));
+
+console.log("========================================================================");
+console.log("========================================================================");
+
+
+// let a = 5;
+
+// if (a%2 == 0) {
+//     console.log("even");
+
+// }else {
+//     console.log("odd");
+
+// }
+
+console.log("========================================================================");
+
+
+// let a = prompt("Enter a 1 for ordering pizza and 2 for burger");
+
+// if (a == 1) {
+//     console.log("You ordered a pizz 🍕");
+
+// }else {
+//     console.log("You ordered a burger 🍔");
+
+// }
+
+
+console.log("========================================================================");
+
+
+
+// if (a == 1) {
+//     console.log("You ordered a pizz 🍕");
+// }else if (a == 2) {
+//     console.log("You ordered a burger 🍔");
+// }else {
+//     console.log("Your choise is wrong ❌");
+
+// }
+
+
+console.log("========================================================================");
+
+
+
+// let age = 20;
+
+// if (age >= 18) {
+//     if (age <= 17) {
+//         console.log("You can drive.");
+//     } else {
+//         console.log("You can't drive.");
+//     }
+// } else {
+//     console.log("You are too young to drive.");
+// }
+
+
+console.log("========================================================================");
+
+
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+
+// if (a <= b) {
+//     if (a <= c) {
+//         console.log("a is min");
+
+//     }else {
+//         console.log("c is min");
+
+//     }
+// }else {
+//     if (b <= c) {
+//         console.log("b is min");
+//     }else {
+//         console.log("c is min");
+//     }
+// }
+
+
+console.log("========================================================================");
+
+
+
+// let a = 12;
+// let b = 35;
+// let c;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log ("a =", a, "b =", b);
+
+
+console.log("========================================================================");
+
+
+
+// let a = 12;
+// let b = 35;
+
+// a = a + b;
+// a = a - b;
+// b = b - a;
+
+// [a,b] = [b,a];
+// console.log(a,b);
+
+
+console.log("========================================================================");
+
+
+let a = prompt("Enter A Chearter");
+
+
+switch (a) {
+    case "A":
+        alert("Vowels");
+        break;
+
+    case "E":
+        alert("Vowels");
+        break;
+
+    case "I":
+        alert("Vowels");
+        break;
+
+    case "O":
+        alert("Vowels");
+        break;
+
+    case "U":
+        alert("Vowels");
+        break;
+
+    case "a":
+        alert("Vowels");
+        break;
+
+    case "e":
+        alert("Vowels");
+        break;
+
+    case "i":
+        alert("Vowels");
+        break;
+
+    case "o":
+        alert("Vowels");
+        break;
+
+    case "u":
+        alert("Vowels");
+        break;
+
+    default:
+        alert("Not vowels")
+
+
+}
+
